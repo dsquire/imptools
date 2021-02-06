ef187ce3fc72a10006c98127f63b89af3486ced2
Danny Squire <danny.squire@ttu.edu>
2021-02-06 16:36:18 -0600

    refactor!: Changed the default imputation id column name to match the mice package. New default is '.imp'.

--- R/stack_mids.R
+++ R/stack_mids.R
@@ -1,11 +1,11 @@
 #' Convert a list of data.frames to a stacked tibble
 #'
 #' @param mids A mice mids object.
-#' @param .setid Set the name of the key column. In cases where there are
-#' more than 1 data.frames the key column increments by 1 for each data set.
-#' Defaults to "stack_id"
+#' @param .setid Set the name of the imputation key column. In cases where there
+#' are more than 1 data.frames the key column increments by 1 for each data set.
+#' Key column name defaults to ".imp".
 #' @export
-stack_mids <- function(mids, .setid = "imp_id") {
+stack_mids <- function(mids, .setid = ".imp") {
   if (is.list(mids))
     dplyr::bind_rows(mids, .id = .setid)
 }
